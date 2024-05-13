807051a1e1b7378bdb5f03c668606ee53c39b138
Michael Agnew <yeahitscheddar@gmail.com>
2024-05-12 20:35:33 -0400

    Started setting up configuration, figuring out UI libraries

---
+++ src/SteeringBoyRP.App/Models/SteeringWheelConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SteeringBoyRP.App.Models
+{
+    [Serializable]
+    public class SteeringWheelConfiguration
+    {
+        public required string WheelImageSourceUri { get; set; }
+        public required string HandImageSourceUri { get; set; }
+        public required string SirenImageSourceUri { get; set; }
+        public bool IsLeftHandEnabled { get; set; }
+        public bool IsRightHandEnabled { get; set; }
+        public bool IsSirenEnabled { get; set; }
+    }
+}
