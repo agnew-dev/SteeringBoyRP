e070f2bd7a2f7ff3a5812e907a11f207dff77cc8
Michael Agnew <yeahitscheddar@gmail.com>
2024-05-13 13:28:48 -0400

    saved edits

--- src/SteeringBoyRP.App/Models/SteeringWheelConfiguration.cs
+++ src/SteeringBoyRP.App/Models/SteeringWheelConfiguration.cs
@@ -7,13 +7,17 @@ namespace SteeringBoyRP.App.Models
     {
         public required string WheelImageSourceUri { get; set; }
         public required string HandImageSourceUri { get; set; }
-        public required string SirenImageSourceUri { get; set; }
 
         public bool IsWheelEnabled { get; set; }
         public bool IsLeftHandEnabled { get; set; }
         public bool IsRightHandEnabled { get; set; }
-        public bool IsSirenEnabled { get; set; }
 
+        public int WheelSizePx { get; set; }
+        public int HandSizePx { get; set; }
 
+        public double LeftHandLocationX { get; set; }
+        public double LeftHandLocationY { get; set; }
+        public double RightHandLocationX { get; set; }
+        public double RightHandLocationY { get; set; }
     }
 }
